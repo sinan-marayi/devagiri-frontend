c3d44c72c062144b3b6b95dfa1ce5d32112c9ba5
sinan-marayi <sinanmarayi8@gmail.com>
2023-02-08 16:15:44 +0530

    hello

--- src/app/dashboard/worker-dashboard/worker-dashboard.component.ts
+++ src/app/dashboard/worker-dashboard/worker-dashboard.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-worker-dashboard',
   templateUrl: './worker-dashboard.component.html',
-  styleUrls: ['./worker-dashboard.component.css']
+  styleUrls: ['./worker-dashboard.component.css'],
 })
 export class WorkerDashboardComponent implements OnInit {
+  constructor(
+    public auth: AuthService,
+    private data: DataService,
+    private router: Router
+  ) {
 
-  constructor(public auth:AuthService,private router:Router) { }
-
-  ngOnInit(): void {
   }
 
-logout(){
-localStorage.clear()
-this.router.navigate(['/'])
-}
+  ngOnInit(): void {}
 
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/']);
+  }
 }
--- src/app/dashboard/workerDashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/workerDashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+profile:any;
+  constructor(private data:DataService) {
+    data.getProfile({id:localStorage.getItem('user_id')}).subscribe(res=>{
+      this.profile=res.data
+      console.log(res);
 
-  constructor() { }
+          });
+  }
 
   ngOnInit(): void {
   }
